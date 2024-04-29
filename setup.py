b1f1f1e74ecc61bdb60ed2813c124dd4daa5ca70
David Juanwu Lu <juanwu@purdue.edu>
2024-04-28 22:02:18 -0400

    env(config) Adds anaconda environment configuration file and PyPI requirements file (#1)

---
+++ setup.py
@@ -0,0 +1,8 @@
+# !/usr/bin/env python
+# Copyright (c) 2024, Juanwu Lu.
+# Released under the BSD 3-Clause License.
+# Please see the LICENSE file that should have been included as part of this
+# project source code.
+from setuptools import setup, find_packages
+
+setup(packages=find_packages())
